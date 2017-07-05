a869a32a65e3026cc81657530f68ab5a715dd232
super12298 <super12298@users.noreply.github.com>
2017-07-05 00:01:47 -0700

    Add files via upload

---
+++ SecureOS-Rewrite/system/boot.lua
@@ -0,0 +1,53 @@
+
+shell.run("/system/images")
+shell.run("/system/config.txt")
+print("[Disclaimer]: This is a beta software!")
+sleep(.5)
+term.clear()
+term.setCursorPos(1,1)
+lnes = 19
+totallnes = lnes*8
+z = 0
+
+function bootfiles()
+BFS.loadimages(totallnes, bootlogo)
+end
+
+function setupandlogin()
+term.clear()
+z = 1
+BFS.loadimages(lnes, loginlogo)
+term.setCursorPos(15,14)
+print(readusern)
+BFS.dply(1,1)
+BFS.tpla(1,2,"[Project Secure] Version:")
+BFS.readfile(26,2,"system/version.txt")
+if debug == "1" then
+write(" [DEV EDITION]  ")
+end
+term.setCursorPos(15,16)
+usrpwd = read("*")
+	if usrpwd == readuserpwd then
+	if not fs.exists( "temp" ) then
+	File = fs.open("temp", "w")
+	File.write(" ")
+	File.close()
+	end
+	shell.run("/system/desk.lua")
+	else
+	setupandlogin()
+	end
+end
+
+local ok,val = pcall(bootfiles)
+if not ok then
+  os.reboot()
+  print("[Error]:",val)
+  sleep(5)
+end
+local ok,val = pcall(setupandlogin)
+if not ok then
+  print("[Error]:",val)
+  sleep(5)
+  os.reboot()
+end
